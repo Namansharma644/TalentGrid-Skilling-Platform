250edd9ad24b7dd4d63d8ddf764ae17c8a350619
Naman sharma <sharma.naman.in@gmail.com>
2026-09-24 22:58:14 +0530

    add security layer at district api

--- src/routers/district.route.js
+++ src/routers/district.route.js
@@ -1,9 +1,13 @@
 const express=require("express");
 const router=express.Router();
+const authMiddleware=require("../middlewares/auth.middleware");
+const roleMiddleware=require("../middlewares/role.middleware");
 const districtController=require("../controllers/district.controller");
 
 router.post(
     "/",
+    authMiddleware,
+    roleMiddleware("government_admin"),
     districtController.createDistrict
 );
 
